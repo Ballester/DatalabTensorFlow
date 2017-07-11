7da70293e14f75b018d5b05dde8bef2eeca1c62b
ubcorrea <sessilu>
2017-07-10 23:20:16 -0300

    Atualização wget -c

--- utils/download_alexnet_weights.py
+++ utils/download_alexnet_weights.py
@@ -1,3 +1,3 @@
 #!-*- conding: utf8 -*-
 import os
-os.system('wget https://www.dropbox.com/s/ntgypjtfrigv08e/bvlc_alexnet.npy -O data/bvlc_alexnet.npy')
+os.system('wget -c https://www.dropbox.com/s/ntgypjtfrigv08e/bvlc_alexnet.npy -O data/bvlc_alexnet.npy')
